763cbe214b7b5e948ec6fad38001f092cfc58f5b
pikri ramdani <ramdanifikri74@gmail.com>
2020-11-08 01:26:11 +0700

    update file python

---
+++ latihan1.py
@@ -0,0 +1,21 @@
+# Latihan 1
+
+print("Tugas Pertemuan 7 \n")
+
+print(list(range(0, 10, 1)))
+print(list(range(1, 11, 1)))
+print(list(range(2, 12, 1)))
+print(list(range(3, 13, 1)))
+print(list(range(4, 14, 1)))
+print(list(range(5, 15, 1)))
+print(list(range(6, 16, 1)))
+print(list(range(7, 17, 1)))
+print(list(range(8, 18, 1)))
+print(list(range(9, 19, 1)))
+print()
+
+print("=========================")
+print("= Nama   : Pikri Ramdan  ")
+print("= NIM    : 312010162     ")
+print("= Kelas  : TI.20 A.1     ")
+print("=========================")
---
+++ latihan2.py
@@ -0,0 +1,16 @@
+n=int(input("Masukan Nilai N :"))
+
+import random
+
+for x in list(range(1,n+1,1)):
+    print(random.uniform(0,0.5))
+
+print()
+
+print("============================")
+print("= Nama   : Pikri Ramdani   =")
+print("= NIM    : 312010162       =")
+print("= Kelas  : TI.20 A.1       =")
+print("============================")
+
+
